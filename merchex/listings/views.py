dbb89d178821908bd18afdc439a42e172f0890e9
Rodolphe <rodolphe.derensy@hotmail.com>
2022-06-12 03:40:54 +0200

    Deuxième page de cours Django

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>À propos de nous</h1> <p>Nous adorons Merch !</p>')
+
+def listing(request):
+    return HttpResponse('<h1>Voici la liste des marchandises :</h1> <p>Tulipes : 15<br/> Roses : 12</p>')
+
+def contact(request):
+    return HttpResponse('<h1>Nous contacter</h1> <p>Notre adresse mail : bullshit@mescouillessurlefront.com</p>')

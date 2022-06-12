2249dfc7edbc763319644cf57c577e076327066c
Rodolphe <rodolphe.derensy@hotmail.com>
2022-06-12 04:58:32 +0200

    Deuxième chapitre de Django

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from listings.models import Band
 
-# Create your views here.
+def hello(request):
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Mes groupes préférés sont :<p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+""")
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
+def listing(request):
+    return HttpResponse('<h1>Liste des produits</h1> <p>Des fleurs.</p>')
+
+def contact(request):
+    return HttpResponse('<h1>Notre mail :</h1> <p>Oui</p>')
